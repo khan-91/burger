adfac75df8f08b0cc00c23b58cff41d0bd264a7a
Tanveer <tanveera915637@gmail.com>
2025-07-16 16:41:49 +0600

    Project SetUp

---
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class BurgerBuilder extends Component {
+  render() {
+    return (
+      <div><p>BurgerBuilder</p></div>
+    )
+  }
+}
+
+export default BurgerBuilder
