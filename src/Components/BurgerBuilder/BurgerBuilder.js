df1225f70077017b9ac8820422365f17ae20b763
Tanveer <tanveera915637@gmail.com>
2025-07-17 12:52:23 +0600

    Add and Remove Ingredients

--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -3,17 +3,46 @@ import Burger from './burger/Burger'
 import Controls from './Controls/Controls'
 export class BurgerBuilder extends Component {
   state = {
-     ingredients: [
-      { type:'salad', ammount: 1 },
-      { type:'cheese', ammount: 1 },
-      { type:'meat', ammount: 2 },
-     ]
+    ingredients: [
+      { type: 'salad', ammount: 0 },
+      { type: 'cheese', ammount: 0 },
+      { type: 'meat', ammount: 0 },
+    ]
   }
+
+  addIngredentHandle = type => {
+    const ingredients = [...this.state.ingredients];
+    for (let item of ingredients) {
+      if (item.type === type) {
+        item.ammount++;
+      }
+      this.setState({ ingredients: ingredients })
+    }
+
+  }
+
+  removeIngredientHandle = type => {
+    const ingredients = [...this.state.ingredients];
+    for (let item of ingredients) {
+      if (item.type === type) {
+        if (item.ammount <= 0) {
+          return;
+        }
+        item.ammount--;
+      }
+      this.setState({ ingredients: ingredients });
+    }
+
+  }
+
   render() {
     return (
       <div className='d-flex flex-md-row flex-column'>
-        <Burger ingredients = {this.state.ingredients} />
-        <Controls />
+        <Burger ingredients={this.state.ingredients} />
+        <Controls
+          ingredientAdded={this.addIngredentHandle}
+          ingredientRemoved={this.removeIngredientHandle}
+        />
       </div>
     )
   }
--- src/Components/BurgerBuilder/Controls/Controls.js
+++ src/Components/BurgerBuilder/Controls/Controls.js
@@ -10,9 +10,9 @@ const controlArray = [
 const BuildControl = props => {
     return (
         <div className='d-flex'>
-            <div className='me-auto ms-5' style={{fontWeight:'bold', fontSize:'1.2rem'}}>{props.label}</div>
-            <Button className='btn btn-danger btn-sm m-1'>Less</Button>
-            <Button className='btn btn-success btn-sm m-1'>More</Button>
+            <div className='me-auto ms-5' style={{ fontWeight: 'bold', fontSize: '1.2rem' }}>{props.label}</div>
+            <Button className='btn btn-danger btn-sm m-1' onClick={props.removed}>Less</Button>
+            <Button className='btn btn-success btn-sm m-1' onClick={props.added}>More</Button>
         </div>
     )
 }
@@ -36,6 +36,8 @@ const Controls = props => {
                                 label={item.label}
                                 type={item.type}
                                 key={Math.random()}
+                                added={() => props.ingredientAdded(item.type)}
+                                removed={() => props.ingredientRemoved(item.type)}
                             />
 
                         })
