7cc47bcc9c53145e951a4a93255a0acc94aba314
Tanveer <tanveera915637@gmail.com>
2025-07-21 16:10:40 +0600

    Adding Validation

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -8,6 +8,7 @@ export class Auth extends Component {
     render() {
         return (
             <div>
+                {/* <h3>Signup</h3> */}
                 <Formik
                     initialValues={
                         {
@@ -21,9 +22,37 @@ export class Auth extends Component {
                             console.log(values);
                         }
                     }
+
+                    validate={(values) => {
+                        const errors = {};
+
+                        if (!values.email) {
+                            errors.email = 'Required';
+                        } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i.test(values.email)) {
+                            errors.email = 'Invalid email address';
+                        }
+
+                        if (!values.password) {
+                            errors.password = 'Required';
+                        } else if (values.password.length < 4) {
+                            errors.password = 'Must be atleast 4 charcters!';
+                        }
+
+                        if (!values.passwordConfirm) {
+                            errors.passwordConfirm = 'Required';
+                        } else if (values.password !== values.passwordConfirm) {
+                            errors.passwordConfirm = 'Password does not match!!!';
+                        }
+                        return errors;
+                    }}
+
                 >
-                    {({values, handleChange, handleSubmit}) => (
-                        <div>
+                    {({ values, handleChange, handleSubmit, errors }) => (
+                        <div style={{
+                            border: "1px solid grey",
+                            padding: "15px",
+                            borderRadius: "5px",
+                        }}>
                             <Form onSubmit={handleSubmit}>
                                 <FloatingLabel
                                     controlId="floatingInput"
@@ -31,22 +60,25 @@ export class Auth extends Component {
                                     className="mb-3"
                                 >
                                     <Form.Control name='email' type="email" placeholder="name@example.com"
-                                    value={values.email}
-                                    onChange={handleChange} />
+                                        value={values.email}
+                                        onChange={handleChange} />
+                                    <span style={{ color: 'red' }}>{errors.email}</span>
                                 </FloatingLabel>
                                 <FloatingLabel controlId="floatingPassword" label="Password"
-                                className="mb-3">
+                                    className="mb-3">
                                     <Form.Control name='password' type="password" placeholder="Password"
-                                    value= {values.password}
-                                    onChange={handleChange} />
+                                        value={values.password}
+                                        onChange={handleChange} />
+                                    <span style={{ color: 'red' }}>{errors.password}</span>
                                 </FloatingLabel>
                                 <FloatingLabel controlId="floatingConfirmPassword" label="Confirm Password"
-                                className="mb-3">
+                                    className="mb-3">
                                     <Form.Control name='passwordConfirm' type="password" placeholder="Confirm Password"
-                                    value= {values.passwordConfirm}
-                                    onChange={handleChange} />
+                                        value={values.passwordConfirm}
+                                        onChange={handleChange} />
+                                    <span style={{ color: 'red' }}>{errors.passwordConfirm}</span>
                                 </FloatingLabel>
-                                <Button type='submit'  style={{ backgroundColor: '#B22222', borderColor:'#B22222'}}>Sign up
+                                <Button type='submit' style={{ backgroundColor: '#B22222', borderColor: '#B22222' }}>Sign up
                                 </Button>
                             </Form>
                         </div>
